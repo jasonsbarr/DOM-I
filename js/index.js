0195fdb3b9d61ec4274726ecabc37087e96e2431
Jason Barr <jason@jasonsbarr.com>
2019-04-10 21:00:15 -0500

    Whoops, didn't mean to do it all in one commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,98 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+/*
+ * Task 1: Selectors
+ */
+
+// Header
+const nav = document.querySelector('header nav');
+const navLinks = document.querySelectorAll('nav a');
+
+// CTA
+const ctaHeader = document.querySelector('.cta h1');
+const ctaBtn = document.querySelector('.cta button');
+const ctaImg = document.querySelector('#cta-img');
+
+// Main content - top content
+const topTextContents = document.querySelectorAll('.top-content .text-content');
+topTextContents[0].id = 'features';
+topTextContents[1].id = 'about';
+const features = document.querySelector('#features');
+const about = document.querySelector('#about');
+
+// Middle img
+const middleImg = document.querySelector('#middle-img');
+
+// Main content - bottom content
+const bottomTextContents = document.querySelectorAll('.bottom-content .text-content');
+bottomTextContents[0].id = 'services';
+bottomTextContents[1].id = 'product';
+bottomTextContents[2].id = 'vision';
+const services = document.querySelector('#services');
+const product = document.querySelector('#product');
+const vision = document.querySelector('#vision');
+
+// Contact
+const contact = document.querySelector('.contact');
+const contactParas = contact.querySelectorAll('p');
+
+// Footer
+const copyright = document.querySelector('footer p');
+
+/*
+ * Task 2: Update HTML
+ */
+// Header
+const navKeys = Object.keys(siteContent.nav).filter(key => key.includes('item'));
+navKeys.forEach((_, i) => {
+  navLinks[i].textContent = siteContent.nav[navKeys[i]];
+});
+
+// CTA
+ctaHeader.innerHTML = siteContent.cta.h1.split(' ').join('<br>');
+ctaBtn.textContent = siteContent.cta.button;
+ctaImg.src = siteContent.cta['img-src'];
+
+// Top Content Section
+features.querySelector('h4').textContent = siteContent["main-content"]["features-h4"];
+features.querySelector('p').textContent = siteContent["main-content"]["features-content"];
+about.querySelector('h4').textContent = siteContent["main-content"]["about-h4"];
+about.querySelector('p').textContent = siteContent["main-content"]["about-content"];
+
+// Middle img
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+// Bottom Content Section
+services.querySelector('h4').textContent = siteContent["main-content"]["services-h4"];
+services.querySelector('p').textContent = siteContent["main-content"]["services-content"];
+product.querySelector('h4').textContent = siteContent["main-content"]["product-h4"];
+product.querySelector('p').textContent = siteContent["main-content"]["product-content"];
+vision.querySelector('h4').textContent = siteContent["main-content"]["vision-h4"];
+vision.querySelector('p').textContent = siteContent["main-content"]["vision-content"];
+
+// Contact
+contact.querySelector('h4').textContent = siteContent.contact["contact-h4"];
+contactParas[0].innerHTML = siteContent.contact.address.split('t S').join('t<br>S');
+contactParas[1].textContent = siteContent.contact.phone;
+contactParas[2].textContent = siteContent.contact.email;
+
+// Copyright
+copyright.textContent = siteContent.footer.copyright;
+
+/*
+ * Add new content
+ */
+navLinks.forEach(link => link.style.color = 'green');
+const home = document.createElement('a');
+home.src = '#';
+home.textContent = 'Home';
+home.style.color = 'green';
+nav.prepend(home);
+
+const design = document.createElement('a');
+design.src = '#';
+design.textContent = 'Design'
+design.style.color = 'green';
+nav.appendChild(design);
